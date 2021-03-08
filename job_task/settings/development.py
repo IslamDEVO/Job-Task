c47860c288d24a41cea609bdf15d0b54d899dc4f
IslamDEVO <i.mohammed14.44@outlook.com>
2021-03-08 21:14:27 +0200

    Add Static Assets

--- job_task/settings/development.py
+++ job_task/settings/development.py
@@ -5,6 +5,10 @@ DEBUG = True
 # Add New Apps To INSTALLED_APPS
 INSTALLED_APPS.append('accounts.apps.AccountsConfig')
 INSTALLED_APPS.append('pages.apps.PagesConfig')
+# Add Third-party
+INSTALLED_APPS.append('crispy_forms')
+# Django Crispy-Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Add Templates Directory Settings
 TEMPLATES[0]["DIRS"] = [BASE_DIR.joinpath('../templates')]
@@ -16,3 +20,15 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 # Tell Django to redirct logged out in user to the homepage
 LOGOUT_REDIRECT_URL = 'home'
+
+# Tell Django Where Static Directory
+STATIC_URL = '/static/'
+# Location of static files in local development
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('../static')),)
+# Location of static files for production, when ues collectstatic command
+STATIC_ROOT = str(BASE_DIR.joinpath('../staticfiles'))
+# which tells django how to look for static file directories
+STATICFILES_FINDERS = [ 
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
